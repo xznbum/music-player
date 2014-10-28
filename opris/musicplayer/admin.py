8747f4d7e90db20c8668624b9c56e24b19d2ccfe
Alexandr Zhukov <alexandrvj@gmail.com>
2014-10-28 17:53:19 +0500

    Создал логическую схему БД

--- opris/musicplayer/admin.py
+++ opris/musicplayer/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from musicplayer.models import *
 # Register your models here.
+
+
+admin.site.register(Compositor)
+admin.site.register(Genre)
+admin.site.register(MusicComposition)
+admin.site.register(Playlist)
+admin.site.register(Comment)
--- opris/musicplayer/models.py
+++ opris/musicplayer/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Compositor(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class MusicComposition(models.Model):
+    compositor = models.ForeignKey(Compositor)
+    name = models.CharField(max_length=200)
+    genre = models.ForeignKey(Genre)
+    duration = models.FloatField()
+    rating_listen = models.IntegerField(default=0)
+    rating_user = models.IntegerField(default=0)
+
+
+class Playlist(models.Model):
+    user = models.ForeignKey(User)
+    name = models.CharField(max_length=200)
+    music_compositions = models.ManyToManyField(MusicComposition)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User)
+    music_composition = models.ForeignKey(MusicComposition)
+    text = models.CharField(max_length=500)
+    rating = models.IntegerField(default=0)
