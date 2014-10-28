cd67f4e18ab79c6de183ef6bdc1dcdc1d3d1a2cb
Alexandr Zhukov <alexandrvj@gmail.com>
2014-10-28 18:24:33 +0500

    Добавил админку

--- opris/musicplayer/admin.py
+++ opris/musicplayer/admin.py
@@ -3,8 +3,22 @@ from musicplayer.models import *
 # Register your models here.
 
 
-admin.site.register(Compositor)
-admin.site.register(Genre)
-admin.site.register(MusicComposition)
+class CompositorAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+class MusicCompositionAdmin(admin.ModelAdmin):
+    list_display = ["compositor", "name", "genre", "duration", "rating_listen", "rating_user"]
+
+
+admin.site.site_header = "Music player"
+admin.site.site_title = "Music player"
+admin.site.register(Compositor, CompositorAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(MusicComposition, MusicCompositionAdmin)
 admin.site.register(Playlist)
 admin.site.register(Comment)
