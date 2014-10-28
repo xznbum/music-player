fc4c52831ddd77ed65181715b630fe2a2cc58902
Alexandr Zhukov <alexandrvj@gmail.com>
2014-10-28 18:53:51 +0500

    Изменил интерфейс админки

--- opris/musicplayer/admin.py
+++ opris/musicplayer/admin.py
@@ -5,14 +5,27 @@ from musicplayer.models import *
 
 class CompositorAdmin(admin.ModelAdmin):
     list_display = ["name"]
+    search_fields = ["name"]
 
 
 class GenreAdmin(admin.ModelAdmin):
     list_display = ["name"]
+    search_fields = ["name"]
 
 
 class MusicCompositionAdmin(admin.ModelAdmin):
-    list_display = ["compositor", "name", "genre", "duration", "rating_listen", "rating_user"]
+    list_display = ["name", "compositor", "genre", "duration", "rating_listen", "rating_user"]
+    search_fields = ["compositor", "name", "genre"]
+
+
+class PlaylistAdmin(admin.ModelAdmin):
+    list_display = ["name", "user"]
+    search_fields = ["user", "name"]
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["text", "rating", "user", "music_composition"]
+    search_fields = ["user", "music_composition", "text"]
 
 
 admin.site.site_header = "Music player"
@@ -20,5 +33,5 @@ admin.site.site_title = "Music player"
 admin.site.register(Compositor, CompositorAdmin)
 admin.site.register(Genre, GenreAdmin)
 admin.site.register(MusicComposition, MusicCompositionAdmin)
-admin.site.register(Playlist)
-admin.site.register(Comment)
+admin.site.register(Playlist, PlaylistAdmin)
+admin.site.register(Comment, CommentAdmin)
